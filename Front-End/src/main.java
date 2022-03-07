bb703ad00f92d8cecd5c662f013b98fb357b6bf9
Nick-Gaudet <ngaudet18@gmail.com>
2022-03-06 19:38:12 -0500

    Login function Created, acccounts load from text file

--- Front-End/src/main.java
+++ Front-End/src/main.java
@@ -1,15 +1,69 @@
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Scanner;
 
-
 public class main {
+
+    private static HashMap<String, String> userAccountsFromFile = new HashMap<String, String>();
+
+
     public boolean post(String city , float price, int bedrooms, boolean rentFlag){
         return false;
     }
+    public static HashMap getUserAccounts(){
+        return userAccountsFromFile;
+    }
+    public static void loadUserAccounts(){ // load user accounts from file and put into hash map
+        File file = new File("Front-End/resources/accounts.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String [] userInfo = line.split("/");
+                // USER NAME STORED IN userInfo[0]
+                // USER PRIVILEGES STORED IN userInfo[1]
+                getUserAccounts().put(userInfo[0], userInfo[1]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean login(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter UserName:  ");
+        String userName = scan.nextLine();
+        if (getUserAccounts().containsKey(userName)){
+            System.out.println("User Found, Logging In...");
+            return true;
+        }
+        else{
+            System.out.println("User Not Found!");
+            String answer= "";
+
+            System.out.println("Would you like to create an account? ");
+            answer = scan.nextLine();
+
+            if (answer.equalsIgnoreCase("no")){
+                System.out.println("Redirecting...");
+                return true;
+            }
+            else if (answer.equalsIgnoreCase("yes")){
+                //create()
+                return true;
+            }
+            return false;
+        }
+
+    }
     public static void main(String[] args){
+        loadUserAccounts();
         System.out.println("Hello World!");
         Post p = new Post("Toronto" , 99.99f, 4, false);
         System.out.println(p.getRentalUnit().getRentalID());
+        login();
     }
+
+
 }
