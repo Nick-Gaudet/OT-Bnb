2e611f844aabf61409c59ae8c07f9bcad9c9152e
KevinC <30735305+KevinChitnaraine@users.noreply.github.com>
2022-04-06 01:07:57 -0400

    Simple test update

--- Front-End/JunitTests/mainTest.java
+++ Front-End/JunitTests/mainTest.java
@@ -19,7 +19,6 @@ class mainTest {
         System.setIn(systemIn);
     }
 
-
     @AfterEach
     public void restoreSystemInputOutput() {
         System.setOut(systemOut);
@@ -28,35 +27,15 @@ class mainTest {
 
     @Test
     public void testCase1() throws IOException {
-        System.setIn(new ByteArrayInputStream("login\nNickG\nlogout\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("login\nKevinCht\nlogout\n".getBytes()));
 
         main.main(new String[]{"accounts.txt", "transactions.txt"});
 
         assertEquals("Welcome to OT-Bnb. Please Login.\n" +
+                "Please enter a command. (Type help for a list of commands): \n" +
                 "Enter UserName:  \n" +
                 "User Found, Logging In...\n" +
+                "Please enter a command. (Type help for a list of commands): \n" +
                 "User logged out. Thank you for using OT-Bnb!\n", testOut.toString());
     }
-/*
-    @Test
-    public void test1() throws IOException {
-        InputStream stdin = System.in;
-        System.setIn(new ByteArrayInputStream("login\nNickG\n".getBytes()));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(byteArrayOutputStream);
-        PrintStream stdout = System.out;
-        System.setOut(ps);
-
-        main.main(new String[]{"accounts.txt", "transactions.txt"});
-
-        System.setIn(stdin);
-        System.setOut(stdout);
-
-        String outputText = byteArrayOutputStream.toString();
-        String key = "output:";
-        String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
-        assertEquals(output, "User Found, Logging In...");
-    }*/
-
 }
