4dfb031ae07ea813663c912323154b594ab63f03
KevinC <30735305+KevinChitnaraine@users.noreply.github.com>
2022-04-06 01:51:00 -0400

    Update test

--- Front-End/JunitTests/mainTest.java
+++ Front-End/JunitTests/mainTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import java.io.*;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,15 +25,15 @@ class mainTest {
 
     @Test
     public void testCase1() throws IOException {
-        System.setIn(new ByteArrayInputStream("login\nKevinCht\nlogout\n".getBytes()));
+        System.setIn(new ByteArrayInputStream("login\nKevinCht\nlogout".getBytes()));
 
         main.main(new String[]{"accounts.txt", "transactions.txt"});
 
-        assertEquals("Welcome to OT-Bnb. Please Login.\n" +
-                "Please enter a command. (Type help for a list of commands): \n" +
-                "Enter UserName:  \n" +
-                "User Found, Logging In...\n" +
-                "Please enter a command. (Type help for a list of commands): \n" +
-                "User logged out. Thank you for using OT-Bnb!\n", testOut.toString());
+        assertEquals("Welcome to OT-Bnb. Please Login.\r" +
+                "Please enter a command. (Type help for a list of commands): \r" +
+                "Enter UserName:  \r" +
+                "User Found, Logging In...\r" +
+                "Please enter a command. (Type help for a list of commands): \r" +
+                "User logged out. Thank you for using OT-Bnb!\r", testOut.toString());
     }
 }
